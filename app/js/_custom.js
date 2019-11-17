4f3a083994d0e541019fecabe00b50bd331f15e7
Sokolovskiy <SumFalcon921@gmail.com>
2019-11-17 21:33:27 +0200

    Fix

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,45 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
+  // Vimeo API
+  var iframe = $("#vimeo-player")[0];
+  var player = $f(iframe);
 
-    // Vimeo API
-    var iframe = $('#vimeo-player')[0];
-    var player = $f(iframe);
+  // Vimeo Pop Up
+  $(".watch-video-btn").click(function() {
+    $(".vimeo-pop-up").show();
+    player.api("play");
+  });
+  // Close Video Pop Up
+  $(".close-btn").click(function() {
+    $(".vimeo-pop-up").hide();
+    player.api("pause");
+  });
 
-    // Mobile Navigation
-    $('.modile-nav').click(function () {
-        $('.header-nav').slideToggle();
-    });
+  // Sign up to waitlist Pop Up
+  $(".banner-link").click(function(e) {
+    e.preventDefault();
+    $(".waitlist-pop-up").show();
+  });
+  // Close Sign up to waitlist Pop Up
+  $(".close-btn").click(function() {
+    $(".waitlist-pop-up").hide();
+  });
 
-    // Vimeo Pop Up
-    $('.watch-video-btn').click(function () {
-        $('.vimeo-pop-up').show();
-        player.api('play');
-    });
-    // Close Video Pop Up
-    $('.close-btn').click(function () {
-        $('.vimeo-pop-up').hide();
-        player.api('pause');
-    });
+  // Historical interest rate with Linen App
+  $(".rates-link").click(function(e) {
+    e.preventDefault();
+    $(".rate-pop-up").show();
+  });
+  // Close Historical interest rate with Linen App
+  $(".close-btn, .rates-btn").click(function() {
+    $(".rate-pop-up").hide();
+  });
+});
 
-    // Sign up to waitlist Pop Up
-    $('.banner-link').click(function (e) {
-        e.preventDefault();
-        $('.waitlist-pop-up').show();
-    });
-    // Close Sign up to waitlist Pop Up
-    $('.close-btn').click(function () {
-        $('.waitlist-pop-up').hide();
-    });
+// Disclaymer
 
-    // Historical interest rate with Linen App
-    $('.rates-link').click(function (e) {
-        e.preventDefault();
-        $('.rate-pop-up').show();
-    });
-    // Close Historical interest rate with Linen App
-    $('.close-btn, .rates-btn').click(function () {
-        $('.rate-pop-up').hide();
-    })
+$(window).scroll(function() {
+  let windowScroll = $(window).scrollTop();
+  if (windowScroll > 1) {
+    $(".mobile-disclaymer").hide();
+  } else $(".mobile-disclaymer").show();
+});
 
+// // Mobile Navigation
+// $(".modile-nav").addClass("closed");
+// console.log($(".modile-nav").hasClass("closed"));
+// $(".modile-nav").click(function() {
+//   if ($(".mobile-nav").hasClass("closed")) {
+//     console.log("closed");
+//   } else if ($(".mobile-nav").hasClass("opened")) {
+//     console.log("opened");
+//   }
+// });
 
-
+// $(".opened").click(function() {
+//   $(".header-nav").hide();
+//   $(this).addClass("closed");
+//   $(this).removeClass("opened");
+// });
+$(".modile-nav").click(function() {
+  console.log("asd");
+    $(".header-nav").toggle();
 });
